5cdfea8c0965406be4cf22988bf91ada4f722411
keeYuc <52500690@qq.com>
2021-04-25 10:34:12 +0800

    ssssZZ Z

--- 1_factory/factory.go
+++ 1_factory/factory.go
@@ -1,1 +1,27 @@
 package factory
+
+import "fmt"
+
+type machine interface {
+	Run()
+}
+
+type car struct{}
+type drone struct{}
+
+func (c *car) Run() {
+	fmt.Println("car run")
+}
+
+func (c *drone) Run() {
+	fmt.Println("drone run")
+}
+
+func get_machine(n int) machine {
+	switch n {
+	case 1:
+		return new(car)
+	default:
+		return new(drone)
+	}
+}
